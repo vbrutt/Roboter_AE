2a244464fad8d94f32f60f17af6a4c0982abd839
Vera <31653380+vbrutt@users.noreply.github.com>
2019-02-06 09:25:54 +0100

    Add files via upload

--- OwnBot.java
+++ OwnBot.java
@@ -1,3 +1,5 @@
+import java.awt.Dimension;
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,8 +14,6 @@ public class OwnBot extends Creature {
 			for (int i = 0; i < d; ++i) {
 				moveForward();
 				observeAround(obs);
-				obs = observe()[0];
-				System.out.println("Type" + obs.type);
 
 			}
 			if (isEnemy(obs)) {
@@ -25,15 +25,46 @@ public class OwnBot extends Creature {
 		}
 	}
 
+	private void defineDirection() {
+		Dimension d = getMapDimensions();
+		Observation[][] data = new Observation[d.width][d.height];
+		Point p = new Point(d.width, d.height);
+
+		Observation N = data[p.x][p.y - 1];
+		Observation S = data[p.x][p.y + 1];
+		Observation E = data[p.x + 1][p.y];
+		Observation W = data[p.x - 1][p.y];
+
+	}
+
+	private Direction getOppositeDirection(Direction currentDirection) {
+		switch (currentDirection) {
+		case NORTH:
+			return Direction.SOUTH;
+		case SOUTH:
+			return Direction.NORTH;
+		case EAST:
+			return Direction.WEST;
+		case WEST:
+			return Direction.EAST;
+		default:
+			break;
+		}
+		return null;
+	}
+
 	private void observeAround(Observation obs) {
 		int count = 0;
-		List<Observation> observed = new ArrayList<>();
+		List<Point> observed = new ArrayList<>();
+		Direction currentDirection = getDirection();
+		System.out.println("current Dir: " + currentDirection);
 		do {
 			turnRight();
 			obs = observe()[0];
-			if (distance(obs.position) - 1 > 0) {
-				System.out.println("oi");
-				observed.add(obs);
+			System.out.println(getDirection());
+			if (distance(obs.position) - 1 > 0 && !(getDirection().equals(getOppositeDirection(currentDirection)))) {
+				observed.add(obs.position);
+				System.out.println(getDirection());
 			}
 			count++;
 
