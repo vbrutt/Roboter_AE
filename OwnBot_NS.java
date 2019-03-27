d5966f2c7abf0dab67f097907353e5ffc02b0eec
vbrutt <31653380+vbrutt@users.noreply.github.com>
2019-03-27 09:15:59 +0100

    Add files via upload

--- OwnBot_NS.java
+++ OwnBot_NS.java
@@ -1,23 +1,83 @@
+import java.util.Random;
+
 public class OwnBot_NS extends Creature {
+
 	@Override
 	public void run() {
 		while (true) {
-			if (!moveForward()) {
-				attack();
-				turnLeft();
+
+			Observation obs = observe()[0];
+			int d = distance(obs.position) - 1;
+			// Move until the far edge
+			for (int i = 0; i < d; ++i) {
+				if (!(moveForward())) {
+					enemySight(obs);
+					// Hit something unexpected!
+					attack();
+					break;
+				}
 			}
+
+			if (obs.classId == Creature.APPLE_CLASS_ID) { // APPLE EATER
+				while (distance(obs.position) > 1) {
+					if (!moveForward()) {
+						attack();
+					}
+				}
+				attack();
+			}
+
+			attack();
+			turn(obs);
+
 		}
+	}
+
+	private void turn(Observation obs) {
+		Random r = new Random();
+		int i = r.nextInt(2) + 1;
+		switch (i) {
+		case 1:
+			turnLeft();
+			attack();
+			break;
+		case 2:
+			turnRight();
+			attack();
+			break;
+		default:
+			break;
+		}
+	}
+
+	protected boolean myMoveForward() {
+		if (!moveForward()) {
+			attack();
+			return false;
+		}
+		return true;
+	}
+
+	public void enemySight(Observation enemy) {
+		int enemyDistance = distance(enemy.position);
+
+		if (enemyDistance == 2) {
+			delay();
+		} else if (enemyDistance == 1) {
+			attack();
+		} else {
+			moveForward();
+		}
+
 	}
 
 	@Override
 	public String getAuthorName() {
-		// TODO Automatisch generierter Methodenstub
-		return null;
+		return "Darwin SDK";
 	}
 
 	@Override
 	public String getDescription() {
-		// TODO Automatisch generierter Methodenstub
-		return null;
+		return "A rover that looks before it moves.";
 	}
 }
